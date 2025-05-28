1026ebd1162a2dcd4d53eb4ab05ff51709c9ab05
Tee <185324008+TeeHit101@users.noreply.github.com>
2025-05-28 15:47:46 +0200

    Update AddressBok.cpp

--- AddressBok.cpp
+++ AddressBok.cpp
@@ -3,16 +3,18 @@
 
 using namespace std;
 
-// Definiera struct för en kontakt
 struct Kontakt {
     string namn;
     string telefon;
     string email;
 };
 
-// Funktion för att lägga till en kontakt i adressboken
-void laggTillKontakt(Kontakt adressbok[], int& antalKontakter) {
-    system("cls");
+void laggTillKontakt(Kontakt adressbok[], int& antalKontakter, int maxKontakter) {
+    if (antalKontakter >= maxKontakter) {
+        cout << "Adressboken är full. Kan inte lägga till fler kontakter.\n";
+        return;
+    }
+    // system("cls"); // Ta bort eller kommentera om du inte är på Windows
     cout << "\nLägg till kontakt\n";
     cout << "Namn: ";
     cin.ignore();
@@ -24,19 +26,21 @@ void laggTillKontakt(Kontakt adressbok[], int& antalKontakter) {
     antalKontakter++;
     cout << "Kontakten har lagts till.\n";
 }
-// Funktion för att visa alla kontakter i adressboken
 
 void visaAdressbok(const Kontakt adressbok[], int antalKontakter) {
-    system("cls");
+    // system("cls");
     cout << "\nAdressbok:\n";
+    if (antalKontakter == 0) {
+        cout << "Inga kontakter att visa.\n";
+        return;
+    }
     for (int i = 0; i < antalKontakter; ++i) {
         cout << "Namn: " << adressbok[i].namn << ", Telefon: " << adressbok[i].telefon << ", Email: " << adressbok[i].email << endl;
     }
 }
 
-// Funktion för att redigera en kontakt i adressboken
 void redigeraKontakt(Kontakt adressbok[], int antalKontakter) {
-    system("cls");
+    // system("cls");
     string redigerNamn;
     cout << "\nAnge namnet på kontakten du vill redigera: ";
     cin.ignore();
@@ -46,7 +50,7 @@ void redigeraKontakt(Kontakt adressbok[], int antalKontakter) {
         if (adressbok[i].namn == redigerNamn) {
             cout << "Ange det nya namnet för kontakten: ";
             getline(cin, adressbok[i].namn);
-            cout << "Ange den nya telefonnumret för kontakten: ";
+            cout << "Ange det nya telefonnumret för kontakten: ";
             getline(cin, adressbok[i].telefon);
             cout << "Ange den nya emailadressen för kontakten: ";
             getline(cin, adressbok[i].email);
@@ -56,7 +60,7 @@ void redigeraKontakt(Kontakt adressbok[], int antalKontakter) {
     }
     cout << "Kontakten '" << redigerNamn << "' hittades inte.\n";
 }
-//main funktion
+
 int main() {
     setlocale(LC_ALL, "Swedish");
 
@@ -74,11 +78,11 @@ int main() {
         cout << "Ange ditt val: ";
         int val;
         cin >> val;
-
+        cin.ignore(); // Viktig!! Ignorera ny rad efter cin >>
 
         switch (val) {
         case 1:
-            laggTillKontakt(adressbok, antalKontakter);
+            laggTillKontakt(adressbok, antalKontakter, MAX_KONTAKTER);
             break;
         case 2:
             visaAdressbok(adressbok, antalKontakter);
